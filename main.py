3207a5a0090f703568e9bc59845ce9f24244937a
Exoutia <jhav365@gmail.com>
2023-07-01 20:02:28 +0530

    adding gitignore and the move function

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame
 from pygame.locals import *
-from pygame.sprite import _Group
+import sys
 
 pygame.init()
 vec = pygame.math.Vector2  # 2 for two dimensional
 
 HEIGHT = 450
 WIDTH = 400
-ACC = 0.5
+ACC = 0.6
 FRIC = -0.12
-FPS = 30
+FPS = 60
 
 FramePerSec = pygame.time.Clock()
 
@@ -19,17 +19,63 @@ pygame.display.set_caption("Game")
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((30, 30))
+        self.surf = pygame.Surface((20, 20))
         self.surf.fill((128, 255, 40))
         self.rect = self.surf.get_rect()
+        
+        self.pos = vec((10, 385))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+    
+    def move(self):
+        self.acc = vec(0, 0)
+        
+        presed_keys = pygame.key.get_pressed()
+        if presed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if presed_keys[K_RIGHT]:
+            self.acc.x = ACC
+        
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+     
+        self.rect.midbottom = self.pos
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.surf = pygame.Surface((WIDTH, 20))
         self.surf.fill((255, 0, 0))
-        self.rect = self.surf.get_rect(center=(WIDTH/2, HEIGHT-20))
+        self.rect = self.surf.get_rect(center=(WIDTH/2, HEIGHT-10))
 
 PT1 = Platform()
-p1 = Player()
+P1 = Player()
 
+all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(P1)
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        displaysurface.fill('black')
+        
+        P1.move()
+        for entity in all_sprites:
+            displaysurface.blit(entity.surf, entity.rect)
+            
+        pygame.display.update()
+        FramePerSec.tick(FPS)
+
+if __name__ == "__main__":
+    main()
