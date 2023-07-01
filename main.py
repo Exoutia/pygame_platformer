16d5a3609ce08820c9681d71d879748edf484255
Exoutia <jhav365@gmail.com>
2023-07-01 18:32:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.locals import *
+from pygame.sprite import _Group
+
+pygame.init()
+vec = pygame.math.Vector2  # 2 for two dimensional
+
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 30
+
+FramePerSec = pygame.time.Clock()
+
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128, 255, 40))
+        self.rect = self.surf.get_rect()
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf.fill((255, 0, 0))
+        self.rect = self.surf.get_rect(center=(WIDTH/2, HEIGHT-20))
+
+PT1 = Platform()
+p1 = Player()
+
